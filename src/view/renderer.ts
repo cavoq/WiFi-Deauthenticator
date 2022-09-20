b213933de723bc86765145884ea06aad5305422e
David Stromberger <Davidstromberger99@gmx.de>
2022-09-20 23:00:25 +0200

    Integrated jquery, fixed bug with client display

--- src/view/renderer.ts
+++ src/view/renderer.ts
@@ -2,66 +2,70 @@
 * Renderer that handles frontend functionality.
 */
 
-const interfaceSelect: HTMLSelectElement = document.getElementById('interface-select') as HTMLSelectElement;
-const refreshBtn: HTMLButtonElement = document.getElementById('refresh-btn') as HTMLButtonElement;
-const randomMacCheckBox: HTMLInputElement = document.getElementById('random-mac-checkbox') as HTMLInputElement;
-const startScanningBtn: HTMLButtonElement = document.getElementById('start-scanning-btn') as HTMLButtonElement;
-const stopScanningBtn: HTMLButtonElement = document.getElementById('stop-scanning-btn') as HTMLButtonElement;
-const accessPointSelect: HTMLSelectElement = document.getElementById('access-point-select') as HTMLSelectElement;
-const startScanningClisBtn: HTMLButtonElement = document.getElementById('start-scanning-clis-btn') as HTMLButtonElement;
-const stopScanningClisBtn: HTMLButtonElement = document.getElementById('stop-scanning-clis-btn') as HTMLButtonElement;
-const clientDiv: HTMLDivElement = document.getElementById('client-div') as HTMLDivElement;
+const interfaceSelect: JQuery<HTMLSelectElement> = $('#interface-select') as JQuery<HTMLSelectElement>;
+const randomMacCheckBox: JQuery<HTMLInputElement> = $('#random-mac-checkbox') as JQuery<HTMLInputElement>;
+const startScanningBtn: JQuery<HTMLButtonElement> = $('#start-scanning-btn') as JQuery<HTMLButtonElement>;
+const refreshBtn: JQuery<HTMLButtonElement> = $('#refresh-btn') as JQuery<HTMLButtonElement>;
+const stopScanningBtn: JQuery<HTMLButtonElement> = $('#stop-scanning-btn') as JQuery<HTMLButtonElement>;
+const accessPointSelect: JQuery<HTMLSelectElement> = $('#access-point-select') as JQuery<HTMLSelectElement>;
+const startScanningClisBtn: JQuery<HTMLButtonElement> = $('#start-scanning-clis-btn') as JQuery<HTMLButtonElement>;
+const stopScanningClisBtn: JQuery<HTMLButtonElement> = $('#stop-scanning-clis-btn') as JQuery<HTMLButtonElement>;
+const clientDiv: JQuery<HTMLDivElement> = $('#client-div') as JQuery<HTMLDivElement>;
 document.addEventListener('DOMContentLoaded', initializeUi);
 
 async function initializeUi() {
   await setInterfaceSelect();
-  await initializeBand();
-  randomMacCheckBox.addEventListener('change', randomMacChangeHandler);
-  refreshBtn.addEventListener('click', setInterfaceSelect);
-  startScanningBtn.addEventListener('click', startScanningHandler);
-  stopScanningBtn.addEventListener('click', stopScanningHandler);
-  startScanningClisBtn.addEventListener('click', startScanningClientsHandler);
-  stopScanningClisBtn.addEventListener('click', stopScanningClientsHandler);
+  initializeBand();
+  randomMacCheckBox.on('change', async function () {
+    await window.API.setInterfaceMac(this.checked);
+  });
+  refreshBtn.on('click', setInterfaceSelect);
+  startScanningBtn.on('click', startScanningHandler);
+  stopScanningBtn.on('click', stopScanningHandler);
+  startScanningClisBtn.on('click', startScanningClientsHandler);
+  stopScanningClisBtn.on('click', stopScanningClientsHandler);
 }
 
-async function initializeBand() {
-  const bands = $('band-field-set').children('input[name="network-band"]');
-  console.log(bands);
+async function setInterfaceSelect() {
+  interfaceSelect.html('<option value="" selected disabled hidden>Choose LAN Adapter</option>');
+  const networkInterfaceControllers: string[] = await window.API.getNetworkInterfaceControllers();
+  for (let i = 0; i < networkInterfaceControllers.length; i += 1) {
+    interfaceSelect.append($('<option>', {
+      value: networkInterfaceControllers[i],
+      text: networkInterfaceControllers[i],
+    }));
+  }
+  interfaceSelect.on('change', async function () {
+    await window.API.setInterfaceSelection(this.value);
+  });
+}
+
+function initializeBand() {
+  const bands: JQuery<HTMLInputElement> = $('#band-field-set').
+    children('input[name="network-band"]') as JQuery<HTMLInputElement>;
   bands.on('change', bandSelectionChangeHandler);
+  const all: JQuery<HTMLInputElement> = $('#band-field-set').
+    children('#all-bands') as JQuery<HTMLInputElement>;
+  all.on('change', function () {
+    if (this.checked) {
+      bands.prop('checked', true);
+    } else {
+      bands.prop('checked', false);
+    }
+    bandSelectionChangeHandler();
+  });
 }
-
 
 function getSelectedBandValues() {
   const bandValues: string[] = [];
-  const bands: JQuery<HTMLInputElement> = $('band-field-set').
+  const bands: JQuery<HTMLInputElement> = $('#band-field-set').
     children('input[name="network-band"]') as JQuery<HTMLInputElement>;
   bands.each(function () {
     if (this.checked) {
       bandValues.push(this.value);
     }
   });
-  console.log(bandValues);
   return bandValues;
-}
-
-async function setInterfaceSelect() {
-  interfaceSelect.innerHTML = "";
-  const networkInterfaceControllers = await window.API.getNetworkInterfaceControllers();
-  for (let i = 0; i < networkInterfaceControllers.length; i += 1) {
-    const opt = document.createElement('option');
-    opt.value = networkInterfaceControllers[i];
-    opt.innerHTML = networkInterfaceControllers[i];
-    interfaceSelect.appendChild(opt);
-  }
-  interfaceSelect.addEventListener('change', interfaceSelectChangeHandler);
-}
-
-async function randomMacChangeHandler() {
-  await window.API.setInterfaceMac(randomMacCheckBox.checked);
-}
-
-async function interfaceSelectChangeHandler() {
-  await window.API.setInterfaceSelection(interfaceSelect.value);
 }
 
 async function bandSelectionChangeHandler() {
@@ -72,13 +76,14 @@ async function bandSelectionChangeHandler() {
 async function startScanningHandler() {
   const selectedBandValues = getSelectedBandValues();
   if (!selectedBandValues) {
+    console.log("hier")
     return;
   }
   if (selectedBandValues.length === 0) {
     await window.MSG.openMessageBox('No network band selected, you need to select at least one network band.');
     return;
   }
-  if (!interfaceSelect.value) {
+  if (!interfaceSelect.val()) {
     await window.MSG.openMessageBox('No network interface controller selected.');
     return;
   }
@@ -91,22 +96,20 @@ async function stopScanningHandler() {
 }
 
 function setAccessPointSelect(accessPoints: string[]) {
-  accessPointSelect.innerHTML = "";
+  accessPointSelect.html('<option value="" selected disabled hidden>Select Accesspoint</option>');
   for (let i = 0; i < accessPoints.length; i += 1) {
-    const opt = document.createElement('option');
-    opt.value = accessPoints[i];
-    opt.innerHTML = accessPoints[i];
-    accessPointSelect.appendChild(opt);
+    accessPointSelect.append($('<option>', {
+      value: accessPoints[i],
+      text: accessPoints[i],
+    }));
   }
-  accessPointSelect.addEventListener('change', accessPointSelectChangeHandler);
-}
-
-async function accessPointSelectChangeHandler() {
-  await window.API.setAccessPointSelection(accessPointSelect.value);
+  accessPointSelect.on('change', async function () {
+    await window.API.setAccessPointSelection(this.value);
+  });
 }
 
 async function startScanningClientsHandler() {
-  if (!accessPointSelect.value) {
+  if (!accessPointSelect.val()) {
     await window.MSG.openMessageBox('No access point selected, you need to select a target.');
     return;
   }
@@ -120,12 +123,12 @@ async function stopScanningClientsHandler() {
 
 function setClientCheckBoxList(clients: string[]) {
   for (let i = 0; i < clients.length; i += 1) {
-    const inp: HTMLInputElement = document.createElement('input');
-    const br: HTMLBRElement = document.createElement('br');
-    inp.type = 'checkbox';
-    inp.value = clients[i];
-    inp.innerHTML = clients[i];
-    clientDiv.appendChild(inp);
-    clientDiv.appendChild(br);
+    clientDiv.append($('<input>', {
+      type: 'checkbox',
+      value: clients[i],
+    })).append($('<label>', {
+      for: clients[i],
+      text: clients[i],
+    })).append($('<br>'));
   }
 }
